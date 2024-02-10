4334f2a9a8e59f8690dad7168573cab3683da2e4
nareha <nareh.agazaryan@yahoo.com>
2024-02-09 22:19:40 -0800

    feat: finalize preliminary profile page

--- frontend/src/components/ProfilePage/ProfilePage.tsx
+++ frontend/src/components/ProfilePage/ProfilePage.tsx
@@ -4,6 +4,7 @@ import './ProfilePage.css';
 const ProfilePage: React.FC = () => {
   return (
     <div className="info-container">
+      <b>User Information</b>
       <p>Name: Mickey Mouse</p>
       <p>Email: mickey@mouse.com</p>
     </div>
