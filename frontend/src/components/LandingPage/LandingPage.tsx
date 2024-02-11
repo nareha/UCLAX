9b304e2d88ecac83a5a3652f75ceb9a7169b47d1
Alan <alanwu100@gmail.com>
2024-02-11 14:52:51 -0800

    feat: POST submission endpoint

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -7,10 +7,41 @@ const apiCall = () => {
   })
 }
 
+enum Destination {
+  LAX = "LAX",
+  UCLA = "UCLA"
+}
+
+type Submission = {
+  interval_start: Date;
+  interval_end: Date;
+  destination: Destination;
+  contact: string;
+};
+
+const postCall = () => {
+  // Example data - random interval_start, interval_end, destination, and contact
+  const submissionData : Submission= {
+    interval_start: new Date('2024-02-10T09:00:00Z'),
+    interval_end: new Date('2024-02-10T10:00:00Z'),
+    destination: Destination.LAX,
+    contact: '@ChadJohnson'
+  };
+
+  axios.post('http://localhost:9000/submission', submissionData)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error('Error posting submission:', error);
+    });
+}
+
 const LandingPage: React.FC = () => {
   return (
     <div>
       <button onClick={apiCall}>Get a message from Mickey!</button>
+      <button onClick={postCall}>Send a sample message to Mickey!</button>
     </div>
   );
 }
