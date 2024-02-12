1c5560a787f055e810f308cbec8993315db40b6f
Yichun <yl16961@g.ucla.edu>
2024-02-12 02:41:51 -0800

    added questions with fields to submission page; added submit button

--- frontend/src/components/SubmissionPage/SubmissionPage.tsx
+++ frontend/src/components/SubmissionPage/SubmissionPage.tsx
@@ -4,8 +4,42 @@ import './SubmissionPage.css';
 
 const SubmissionPage: React.FC = () => {
   return (
-    <div className="test">
-      hello world
+    <div className="center">
+      <div className="title-logo">
+        <p>Want to Share a Ride?</p>
+      </div>
+      <div className="description">
+        <p>Submit the following information to get your match</p>        
+        <table>
+            <tr>
+                <td><label> Earliest Time of Arrival*: </label></td>
+                <td><input type="datetime-local" /> </td>
+            </tr>
+            <tr>
+                <td><label> Latest Time of Arrival*: </label></td>
+                <td><input type="datetime-local" /> </td>
+            </tr>
+            <tr>
+                <td><label> Trip Start Location*: </label></td>
+                <td><input type="text" /> </td>
+            </tr>
+            <tr>
+                <td><label> Trip Destination*: </label></td>
+                <td><input type="text" /> </td>
+            </tr>
+            <tr>
+                <td><label> Preferred Contact Method*: </label></td>
+                <td><input type="text" /> </td>
+            </tr>
+            <tr>
+                <td><label> Maximum Party Capacity: </label></td>
+                <td><input type="number" /> </td>
+            </tr>
+        </table>
+      </div>
+      <div className="submit">
+        <button>Submit</button>
+      </div> 
     </div>
   );
 }
