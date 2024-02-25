ab2ffd0674b61e7fe26eaf5b916d00f83b7a5973
steveez123 <stevenzhang0808@gmail.com>
2024-02-25 15:44:13 -0800

    feat: Sign-in Button with Google OAuth and working UCLA email authentication (#47)
    * Added Login Button with Google OAuth and working
    UCLA authentication.
    
    * removed unecessary imports
    
    * fix: linting
    
    ---------
    
    Co-authored-by: Alan Wu <60207036+alanhwu@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,12 @@ import ProfilePage from './components/ProfilePage/ProfilePage';
 import NotFoundPage from './components/404Page/404Page';
 import SubmissionPage from './components/SubmissionPage/SubmissionPage';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 const App: React.FC = () => {
   return (
     <>
+    <GoogleOAuthProvider clientId="855516960445-ue7h075jd8jcp9jukvtbgd235sr29q27.apps.googleusercontent.com">
       <Navbar />
       <Routes>
   	    <Route path="/" element={<LandingPage />} />
@@ -17,6 +20,7 @@ const App: React.FC = () => {
         <Route path="/submit" element={<SubmissionPage />} />
   	    <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      </GoogleOAuthProvider>
     </>
   );
 }
--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { useGoogleLogin } from '@react-oauth/google';
+
 import './LandingPage.css';
 
 const apiCall = () => {
@@ -46,6 +48,43 @@ const postCall = () => {
 }
 
 const LandingPage: React.FC = () => {
+  let loggedIn = false;
+  let userInfo = {
+    email:"",
+    email_verified:"",
+    hd:"",
+    name:""
+  }
+  
+  const googleLogin = useGoogleLogin ({
+    onSuccess: async tokenResponse => {
+      // fetching userinfo can be done on the client or the server
+      userInfo = await axios
+        .get('https://www.googleapis.com/oauth2/v3/userinfo', {
+          headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+        })
+        .then(res => res.data);
+      
+      console.log(userInfo);
+      if (!('email' in userInfo) || !('email_verified' in userInfo) || !('name' in userInfo) ){ 
+        throw new Error('Unable to login with this account');
+      }
+      if (!('hd' in userInfo) ){
+        throw new Error('User must sign in with UCLA email');
+      }
+
+      loggedIn = true;
+
+      console.log(userInfo.email)
+      if (!userInfo.email_verified) {
+        throw new Error('Email is not verified');
+      }
+      if (userInfo.hd != "g.ucla.edu") {
+        throw new Error('Not a UCLA Email');
+      }
+    },
+    onError: errorResponse => console.log(errorResponse)
+  });
   return (
     <div>
       <div className="elems">
@@ -54,7 +93,9 @@ const LandingPage: React.FC = () => {
         </div>
         <div className="description">
             <p>Verify to start moving</p>
-            <button>Log in with Google</button>
+            <h1>
+            <button onClick={ () => googleLogin() }>Sign in with Google</button>
+            </h1>
         </div>
       </div>
 
