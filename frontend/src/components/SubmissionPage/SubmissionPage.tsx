2f1fc0613c52d8bb6fe3e532f0f027b786ffb89a
Joyce Tung <joycetung2002@gmail.com>
2024-03-06 23:32:30 -0800

    feat: add hooks to submission form

--- frontend/src/components/SubmissionPage/SubmissionPage.tsx
+++ frontend/src/components/SubmissionPage/SubmissionPage.tsx
@@ -1,46 +1,105 @@
-import React from 'react';
-import SubmissionField , { SubmissionValue } from '../SubmissionField/SubmissionField';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import './SubmissionPage.css';
 
-const submissionValues: Array<SubmissionValue> = [
-  {
-    label: "Earliest Time of Arrival*:",
-    type: "datetime-local"
-  },
-  {
-    label: "Latest Time of Arrival*:",
-    type: "datetime-local"
-  },
-  {
-    label: "Trip Start Location*:",
-    type: "text"
-  },
-  {
-    label: "Trip Destination*:",
-    type: "text"
-  },
-  {
-    label: "Preferred Contact Method*:",
-    type: "text"
-  },
-  {
-    label: "Maximum Party Capacity:",
-    type: "number"
-  }
-];
-
 const SubmissionPage: React.FC = () => {
+  const [formData, setFormData] = useState({
+    "early_time": "",
+    "late_time": "",
+    "source": "UCLA",
+    "dest": "UCLA",
+    "contact": "",
+    "capacity": undefined
+  });
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="page">
       <div className="text">
         <p className="title-text">Want to Share a Ride?</p>
         <p>Submit the following information to get your match.</p>        
       </div>
-      <table>
-        {submissionValues.map(({label, type}) => <SubmissionField label={label} type={type} />)}
-      </table>
-      <button>Submit</button>
+      <form onSubmit={handleSubmit}>
+        <table>
+          <tbody>
+            <tr>
+              <td><label>Earliest Time of Arrival*:</label></td>
+              <td>
+                <input
+                type="datetime-local"
+                name="early_time"
+                value={formData.early_time}
+                onChange={handleChange}
+                />
+              </td> 
+            </tr>
+            <tr>
+              <td><label>Latest Time of Arrival*:</label></td>
+              <td>
+                <input
+                type="datetime-local"
+                name="late_time"
+                value={formData.late_time}
+                onChange={handleChange}
+                />
+              </td> 
+            </tr>
+            <tr>
+              <td><label>Trip Start Location*:</label></td>
+              <td>
+                <select name="source" onChange={handleChange}>
+                  <option value="UCLA">UCLA</option>
+                  <option value="LAX">LAX</option>
+                  <option value="BUR">BUR</option>
+                </select>
+              </td> 
+            </tr>
+            <tr>
+              <td><label>Trip Destination*:</label></td>
+              <td>
+                <select name="dest" onChange={handleChange}>
+                  <option value="UCLA">UCLA</option>
+                  <option value="LAX">LAX</option>
+                  <option value="BUR">BUR</option>
+                </select>
+              </td> 
+            </tr>
+            <tr>
+              <td><label>Contact:</label></td>
+              <td>
+                <input
+                type="text"
+                name="contact"
+                value={formData.contact}
+                onChange={handleChange}
+                />
+              </td> 
+            </tr>
+            <tr>
+              <td><label>Maximum Party Capacity:</label></td>
+              <td>
+                <input
+                type="number"
+                name="capacity"
+                value={formData.capacity}
+                onChange={handleChange}
+                />
+              </td> 
+            </tr>
+          </tbody>
+        </table>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
