828b1bdc3c810e8988bd505d2200d8d131212ac4
Joyce Tung <joycetung2002@gmail.com>
2024-03-13 18:58:46 -0700

    docs: Documentation for express endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,6 +18,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Mickey's Express Server. Have a wonderful day!");
 });
 
+/**
+ * The table GET endpoint lets users access the set of submitted
+ * rideshares.
+ */
 app.get("/table", (req: Request, res: Response) => {
   const submissions = querySubmissions();
   submissions.then((result) => {
@@ -29,6 +33,11 @@ app.get("/table", (req: Request, res: Response) => {
   });
 });
 
+/**
+ * The email GET endpoint is called by the frontend on every user submission.
+ * It is used to send emails notifying users of matching submissions. Every
+ * email retrieved from this endpoint is sent to the user.
+ */
 app.get("/email", (req: Request, res: Response) => {
   let param = req.query.user_id as string;
   const submissionPromise = getEmail(parseInt(param));
@@ -37,6 +46,10 @@ app.get("/email", (req: Request, res: Response) => {
   });
 });
 
+/**
+ * The submission POST endpoint is called by the frontend on every user submission.
+ * It is used to add a new submission for a user, or modify a user's old submission.
+ */
 app.post("/submission", (req: Request, res: Response) => {
   //make sure that the body of the request has the data that we want
   //parse the body of the request and store as a Submission
@@ -54,6 +67,11 @@ app.post("/submission", (req: Request, res: Response) => {
   });
 });
 
+/**
+ * The user POST endpoint is called on successful user signin with Google SSO.
+ * If the user is new, they're added to the users table. Returns the user's id
+ * in the response.
+ */
 app.post("/user", (req: Request, res: Response) => {
   let email = req.body.email;
   console.log("Received: ", email);
